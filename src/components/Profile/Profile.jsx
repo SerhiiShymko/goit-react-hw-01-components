0fae2f528c5c408cfbafe2419ef55e5cf404e5c3
SerhiiShymko <shimko456@gmail.com>
2023-04-25 16:46:20 +0300

    profile_components

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,37 +1,45 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 export const Profile = ({ events }) => {
   return (
     <div className="profile">
       <div className="description">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-          className="avatar"
-        />
-        <p className="name">Petra Marica</p>
-        <p className="tag">@pmarica</p>
-        <p className="location">Salvador, Brasil</p>
+        <img src={events.avatar} alt="User avatar" className="avatar" />
+        <p className="name">{events.username}</p>
+        <p className="tag">{events.tag}</p>
+        <p className="location">{events.location}</p>
       </div>
 
       <ul className="stats">
         <li>
           <span className="label">Followers</span>
-          <span className="quantity">1000</span>
+          <span className="quantity">{events.stats.followers}</span>
         </li>
         <li>
           <span className="label">Views</span>
-          <span className="quantity">2000</span>
+          <span className="quantity">{events.stats.views}</span>
         </li>
         <li>
           <span className="label">Likes</span>
-          <span className="quantity">3000</span>
+          <span className="quantity">{events.stats.likes}</span>
         </li>
       </ul>
     </div>
   );
 };
 
-Profile.propTypes = {
-  events: PropTypes.string.isRequired,
-};
+// Profile.propTypes = {
+//   events: PropTypes.arrayOf(
+//     PropTypes.exact({
+//       username: PropTypes.string.isRequired,
+//       tag: PropTypes.string.isRequired,
+//       location: PropTypes.string.isRequired,
+//       avatar: PropTypes.any.isRequired,
+//       stats: {
+//         followers: PropTypes.number.isRequired,
+//         views: PropTypes.number.isRequired,
+//         likes: PropTypes.number.isRequired,
+//       },
+//     })
+//   ),
+// };
