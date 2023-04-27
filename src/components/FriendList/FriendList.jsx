f57ea8749cb34dd446c9c7343fea3a3051782176
SerhiiShymko <shimko456@gmail.com>
2023-04-27 19:44:54 +0300

    FriendListItem

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,11 @@
+// import css from './FriendList.module.css';
+// import PropTypes from 'prop-types';
+
+// export const FriendList = ({ friends }) => {
+//     return (<ul class="friend-list">
+//         {friends.map(({ id, isOnline, avatar, name  }) =>
+//   )
+//     };
+//         </ul>
+//         )
+// };
---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,18 @@
+import css from './FriendListItem.module.css';
+import PropTypes from 'prop-types';
+
+export const FriendListItem = ({ isOnline, avatar, name }) => {
+  return (
+    <li className={css.item}>
+      <span className={isOnline ? css.statusOnline : css.ctatusOffline}></span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}></p>
+    </li>
+  );
+};
+
+FriendListItem.prototype = {
+  isOnline: PropTypes.bool.isRequired,
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
